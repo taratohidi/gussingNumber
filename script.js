3bfa748487ead308f600d849ec24d5010bd8c481
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-02 15:24:34 -0700

    Complete Check button addEventListener

--- script.js
+++ script.js
@@ -5,24 +5,39 @@ const check = document.querySelector('.check');
 const guess = document.querySelector('.guess');
 const message = document.querySelector('.message');
 const number = document.querySelector('.number');
+const score = document.querySelector('.score');
 
 // Define Random Number
 const numberRandom = Math.trunc(Math.random() * 30) + 1;
 number.textContent = numberRandom;
 
-//Implement the game logic
+let scoreInit = 20;
+
+//Implement the game logic and score
 check.addEventListener('click', event => {
   const guessValue = Number(guess.value);
 
   if (!guessValue) {
-    message.textContent = 'No number, Please enter a number! 🚫';
+    message.textContent = 'No Number 🚫';
+  } else if (guessValue === 0 || guessValue > 30) {
+    message.textContent = 'Between 1 and 30 🚫';
   } else if (guessValue === numberRandom) {
     message.textContent = 'Correct Number! 🎊';
   } else if (guessValue > numberRandom) {
-    message.textContent = 'Less! ⬇️';
+    if (scoreInit >= 1) {
+      score.textContent = scoreInit;
+      message.textContent = 'Less! ⬇️';
+      scoreInit--;
+    } else {
+      message.textContent = 'You lost the game';
+    }
   } else if (guessValue < numberRandom) {
-    message.textContent = 'More! ⬆️';
-  } else if (guessValue === 0 || guessValue > 30) {
-    message.textContent = 'Please enter a number between 1 and 30! ';
+    if (scoreInit >= 1) {
+      score.textContent = scoreInit;
+      message.textContent = 'More! ⬆️';
+      scoreInit--;
+    } else {
+      message.textContent = 'You lost the game';
+    }
   }
 });
