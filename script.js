45f883d8cb8d1dcacbbd2dfa9a3ca73811e0c079
Tara Tohidi <tara.tohidi@gmail.com>
2022-08-02 15:00:18 -0700

    Implement game logic

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
 'use strict';
+
+// Select Elements
+const check = document.querySelector('.check');
+const guess = document.querySelector('.guess');
+const message = document.querySelector('.message');
+const number = document.querySelector('.number');
+
+// Define Random Number
+const numberRandom = Math.trunc(Math.random() * 30) + 1;
+number.textContent = numberRandom;
+
+//Implement the game logic
+check.addEventListener('click', event => {
+  const guessValue = Number(guess.value);
+
+  if (!guessValue) {
+    message.textContent = 'No number, Please enter a number! 🚫';
+  } else if (guessValue === numberRandom) {
+    message.textContent = 'Correct Number! 🎊';
+  } else if (guessValue > numberRandom) {
+    message.textContent = 'Less! ⬇️';
+  } else if (guessValue < numberRandom) {
+    message.textContent = 'More! ⬆️';
+  } else if (guessValue === 0 || guessValue > 30) {
+    message.textContent = 'Please enter a number between 1 and 30! ';
+  }
+});
